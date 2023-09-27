c262c6e8197a5fcdb1d18a9476699ab94ae52e33
Rebecca Feng <beckyfeng08@@berkeley.edu>
2023-09-26 19:12:05 -0700

    added water script

---
+++ GDD_Project_Flameo/Assets/Scripts/Water.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Water : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The player object")]
+    GameObject Player;
+    #endregion
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == Player) {
+            SceneManager.LoadScene("Level1");
+        }
+    }
+}
